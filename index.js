7070d71bf9944c459258dfabb25c7870a77e5bc5
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 17:41:04 -0500

    Tried to fix port

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ catch(err)
 // Create an express server and a GraphQL endpoint
 var app = express();
 var event = require('./event.routes.js')(app);
-app.listen(8081, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+app.listen(process.env.PORT || 5000, () => console.log('Express GraphQL Server Now Running On heroku (https://raggioeventplan.herokuapp.com)' ));
