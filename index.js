16e803a55e04a564d00767785694864f05e69f08
Kwame Baah-Apraku <Kwame.Baah-Apraku@aarcorp.com>
2019-05-18 17:33:20 -0500

    Removed Cors options

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ var express = require('express')
 ,cors = require('cors')
   , app = express();
 
-  var originsWhitelist = [
-    'http://localhost:4200/locations',      //this is my front-end url for development
-     'http://www.myproductionurl.com'
-  ];
-  var corsOptions = {
-    origin: function(origin, callback){
-          var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-          callback(null, isWhitelisted);
-    },
-    credentials:true
-  }
-
-  app.use(cors(corsOptions));
 //Setup Database
 var mongose = require('mongoose');
 try
